821596727368536b6acc5e6dcc4f3a82e51782b1
itodaiber <st147783@stud.uni-stuttgart.de>
2018-11-28 16:47:06 +0100

    use bsub for remote execution

--- runOnHerkules.sh
+++ runOnHerkules.sh
@@ -4,4 +4,4 @@ pathRemote="/home/itodaiber/R - Repositories/tmp/"
 
 ./copyToHerkules.sh
 
-ssh herkules "cd '$pathRemote'; python3.4 main.py"
+ssh herkules "cd '$pathRemote'; bsub 'python3.4 main.py'"

18bbdf941c832a3da947c0a9831c53133151c923
itodaiber <st147783@stud.uni-stuttgart.de>
2018-12-07 11:36:42 +0100

    apply PEP8

--- copyToHerkules.py
+++ copyToHerkules.py
@@ -11,8 +11,6 @@ import os
 import zipfile
 import paramiko
 
-
-
 # ---------------------------------------------
 # settings
 # ---------------------------------------------
@@ -20,7 +18,7 @@ import paramiko
 # zip settings
 zip_Settings = {'zip_File_Name': "herkules.zip",
                 'zip_Include_Folder_List': ["config", "lib"],
-                'zip_Include_File_List': ["main.py", "copyToHerkules.py"],
+                'zip_Include_File_List': ["main.py"],
                 'skipped_Folders': [".git", "__pycache__"]}
 
 # sftp settings
@@ -29,13 +27,12 @@ zip_Settings = {'zip_File_Name': "herkules.zip",
 # - generate an OpenSSH key
 # - save as ~/.ssh/herkules
 # - add the public key to the .ssh/authorized_keys file on the server
-# 
 #
 # __Hint__
-# 
+#
 # PuTTY
 #   Conversions -> Export OpenSSH key
-# 
+#
 
 user_Dir = os.path.expanduser("~")
 key_file_path = os.path.normpath("{}/.ssh/herkules".format(user_Dir))
@@ -48,7 +45,7 @@ sftp_Settings = {'host': "herkules.ito.uni-stuttgart.de",
 
 # additional execute after zipping and copying
 # For example:
-#execute = "bsub 'python3.4 main.py'"
+# execute = "bsub 'python3.4 main.py'"
 execute = ""
 
 # ---------------------------------------------
@@ -58,7 +55,7 @@ execute = ""
 
 def zip_Project(zip_Settings):
     """ Zips certain files and folders into a zip file.
-    
+
     # Arguments
         zip_Settings
             Contains all settings related the zipping process. For example
@@ -68,12 +65,12 @@ def zip_Project(zip_Settings):
     zip_Include_Folder_List = zip_Settings['zip_Include_Folder_List']
     zip_Include_File_List = zip_Settings['zip_Include_File_List']
     zip_File_Name = zip_Settings['zip_File_Name']
-    
+
     skipped_Folders = zip_Settings['skipped_Folders']
-    
+
     def zip_dir(path, zip_h):
         """zip all folder in path, exept those mentioned in _skippedFolders_
-        
+
         # Arguments
             path
                 folder path to zip
@@ -83,17 +80,17 @@ def zip_Project(zip_Settings):
         for root, dirs, files in os.walk(path):
             skip = False
             for f in skipped_Folders:
-                if root.find(f)!=-1:
+                if root.find(f) != -1:
                     skip = True
-            if skip==False:
+            if skip is False:
                 for file in files:
                     zip_h.write(os.path.join(root, file),
-                               os.path.relpath(os.path.join(root, file),
-                                               os.path.join(path, '..')))
-    
+                                os.path.relpath(os.path.join(root, file),
+                                os.path.join(path, '..')))
+
     def zip_File(file, zip_h):
         """zip file
-        
+
         # Arguments
             file
                 file to zip
@@ -101,10 +98,10 @@ def zip_Project(zip_Settings):
                 zip handle
         """
         zip_h.write(file, file)
-    
+
     def zip_it(dir_list, file_list, zip_name):
         """zip specific folders and files
-        
+
         # Arguments
             dir_list
                 list of folders to zip
@@ -119,73 +116,69 @@ def zip_Project(zip_Settings):
         for f in file_list:
             zip_File(f, zip_f)
         zip_f.close()
-    
-    zip_it(zip_Include_Folder_List, zip_Include_File_List, zip_File_Name)
 
+    zip_it(zip_Include_Folder_List, zip_Include_File_List, zip_File_Name)
 
 
 def copy_To_Server(sftp_Settings, execute=""):
     """Copies a zip file via sftp to a folder on a server.
-    
+
     # Arguments
         sftp_Settings
             Contains all settings related the copy process to a server.
             For example host, port and user name.
-        
+
         execute
             Command to be called after copying and unzipping the file.
     """
     host = sftp_Settings['host']
     port = sftp_Settings['port']
-    
+
     user_Name = sftp_Settings['user_Name']
     key_File = sftp_Settings['key_File']
-    
+
     path_Remote = sftp_Settings['path_Remote']
-    
+
     file_Name = sftp_Settings['file']
-    
+
     # open connection
-    ssh_client=paramiko.SSHClient()
-    ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy()) # trust all hosts
+    ssh_client = paramiko.SSHClient()
+    # trust all hosts
+    ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     ssh_client.connect(hostname=host,
                        port=port,
                        username=user_Name,
                        key_filename=key_File)
-    
-    
+
     # creat the directory
-    stdin,stdout,stderr=ssh_client.exec_command("mkdir {}".format(path_Remote))
-    #stdin,stdout,stderr=ssh_client.exec_command("cd ./tmp && ls")
-    #stdin,stdout,stderr=ssh_client.exec_command("ls")
-    
+    command = "mkdir {}".format(path_Remote)
+    stdin, stdout, stderr = ssh_client.exec_command(command)
+
     for l in stdout.readlines():
         print(l[:-1])
-    
-    
+
     # transfer the ziped project
-    ftp_client=ssh_client.open_sftp()
+    ftp_client = ssh_client.open_sftp()
     ftp_client.put(file_Name, "{}{}".format(path_Remote, file_Name))
     ftp_client.close()
-    
-    
+
     # unzip project
     command = "cd {pathRemote}\
                && unzip -q -o '{zipFileName}'\
                && rm '{zipFileName}'"
-               
+
     if execute != "":
         command += "&& {additionalExecute}"
-                    
+
     command = command.format(pathRemote=path_Remote,
                              zipFileName=file_Name,
                              additionalExecute=execute)
-    
-    stdin,stdout,stderr = ssh_client.exec_command(command)
-    
+
+    stdin, stdout, stderr = ssh_client.exec_command(command)
+
     for l in stdout.readlines():
         print(l[:-1])
-    
+
     ssh_client.close()
 
 

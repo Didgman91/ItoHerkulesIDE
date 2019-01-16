8b46f5284ea26ab2d5a2987f4d685ed2addb36e2
itodaiber <st147783@stud.uni-stuttgart.de>
2019-01-16 22:26:39 +0100

    add function to generate a latex document; add plot wrapper functions

--- lib/latex/documentation.py
+++ lib/latex/documentation.py
@@ -6,3 +6,12 @@ Created on Wed Nov 21 22:13:28 2018
 @author: maxdh
 """
 
+from lib.toolbox import toolbox
+
+
+def generate_documentation():
+    toolbox.run_process("pdflatex",
+                        ["-synctex=1",
+                         "-interaction=nonstopmode",
+                         "\"main\".tex"],
+                         working_dir="/lib/latex/document")
--- test.py
+++ test.py
@@ -18,28 +18,115 @@ from script import memory_effect as me
 import matplotlib.pyplot as plt
 
 
-# test
+from lib.latex import documentation
 
-path = ["./data/memory_effect/input/same_fog/0,0/",
-        "./data/memory_effect/input/same_fog/5,0/",
-        "./data/memory_effect/input/same_fog/10,0/"]
 
-shift = me.evaluate_data(path)
-shift = np.stack(shift)
-f = plt.figure()
+#from lib.module_base import module_base
+#
+#
+#
+#class test_module(module_base.module_base):
+#    def __init__(self, **kwds):
+#        super(test_module, self).__init__(**kwds)
+#
+#test = test_module(name="test_module")
 
-for i in range(len(shift)):
-    l = shift[i,:,1]
-    plt.plot(l, label="{} mm".format(i*5))
 
-plt.title("x-shift")
-plt.xlabel("fog / m")
-plt.ylabel("calculated shift / mm")
-plt.legend(loc="down right")
 
-plt.show()
+## ------------------------------mod tex files (add plots) ------------------------------
+#path_plot = "lib/latex/document/tex/plot/plot.tex"
+#toolbox.copy("lib/latex/document/tex/ORG_plot.tex", path_plot)
+#
+#d = {'py_file_name':'shift_5.csv',
+#     'py_addlegend': 'shift 5\\,mm'}
+#path_addplot_1 = "lib/latex/document/tex/plot/plot_addplot_{}.tex".format(1)
+#toolbox.copy("lib/latex/document/tex/ORG_plot_addplot.tex", path_addplot_1)
+#toolbox.replace_in_file(path_addplot_1, d)
+#
+#
+#d = {'py_addplot':"\\input{" + path_addplot_1.replace("lib/latex/document/", "") + "}" + "\npy_addplot"}
+#toolbox.replace_in_file(path_plot, d)
+#
+#d = {'py_file_name':'shift_10.csv',
+#     'py_addlegend': 'shift 10\\,mm'}
+#path_addplot_2 = "lib/latex/document/tex/plot/plot_addplot_{}.tex".format(2)
+#toolbox.copy("lib/latex/document/tex/ORG_plot_addplot.tex", path_addplot_2)
+#toolbox.replace_in_file(path_addplot_2, d)
+#
+#d = {'py_addplot':"\\input{" + path_addplot_2.replace("lib/latex/document/", "") + "}"}
+#toolbox.replace_in_file(path_plot, d)
+#
+#documentation.generate_documentation()
+## mod tex -------------------------------------------------------------------------------
 
-# ~test
+
+
+## test: Memory effect -------------------------------------------------------------------
+#
+#path = ["./data/memory_effect/input/same_fog/0,0/",
+#        "./data/memory_effect/input/same_fog/5,0/",
+#        "./data/memory_effect/input/same_fog/10,0/",
+#        "./data/memory_effect/input/same_fog/15,0/",
+#        "./data/memory_effect/input/same_fog/20,0/",
+#        "./data/memory_effect/input/same_fog/25,0/",
+#        "./data/memory_effect/input/same_fog/30,0/",
+#        "./data/memory_effect/input/same_fog/35,0/",
+#        "./data/memory_effect/input/same_fog/40,0/",
+#        "./data/memory_effect/input/same_fog/45,0/"]
+#
+#shift = me.evaluate_data(path)
+#shift = np.stack(shift)
+#
+#
+#for i in range(len(shift)):
+#    l = shift[i,:,1]
+#    
+#    export = []
+#    for ii in range(len(l)):
+#        export += [np.array([ii+1, l[ii]])]
+#    
+#    export = np.stack(export)
+#    
+#    np.savetxt("data/memory_effect/shift_{}.csv".format(i*5),
+#               export,
+#               delimiter = ',',
+#               header='fog / m,x-shift',
+#               comments='')
+#    
+#
+#
+
+
+#f = plt.figure()
+#
+#for i in range(len(shift)):
+#    l = shift[i,:,1]
+#    plt.plot(l, label="{} mm".format(i*5))
+#
+#plt.title("x-shift")
+#plt.xlabel("fog / m")
+#plt.ylabel("calculated shift / mm")
+#plt.legend(loc="down right")
+#
+#plt.show()
+#f.savefig("x-shift.pdf", bbox_inches="tight")
+#
+#
+#f = plt.figure()
+#for i in range(len(shift)):
+#    l = shift[i,:,0]
+#    plt.plot(l, label="{} mm".format(i*5))
+#
+#plt.title("y-shift")
+#plt.xlabel("fog / m")
+#plt.ylabel("calculated shift / mm")
+#plt.legend(loc="down right")
+#
+#plt.show()
+#f.savefig("y-shift.pdf", bbox_inches="tight")
+
+
+## ~test-------------------------------------------------------------------------------------
 
 #
 #def plot(c, title):

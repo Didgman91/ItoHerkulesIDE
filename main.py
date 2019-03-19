cfa6f6e5a6a1ded990094b406cd44bc3f03bc1f5
itodaiber <st147783@stud.uni-stuttgart.de>
2019-03-19 10:22:55 +0100

    adjust thick scatter plate simulation
    add variable scatter plate thickness

--- main.py
+++ main.py
@@ -6,13 +6,19 @@ Created on Tue Nov 13 15:22:54 2018
 @author: maxdh
 """
 
-import time
+#import time
 
 from lib.toolbox import toolbox
 
 import script.memory_effect as me
 
-import script.fog_simulation as fs
+#import script.fog_simulation as fs
+
+#import script.neuronal_network_todo as nn
+
+#import script.histogram as hg
+
+#from lib.f2 import f2
 
 # By calling a module, e.g. F2_main(), the folder name is written to this list.
 # This folder is located in the "data" folder and contains all data of the
@@ -23,6 +29,28 @@ executed_modules = []
 
 m = me.memory_effect()
 executed_modules = m.run()
+
+
+#path = "data/f2/output/speckle"
+#folder = toolbox.get_subfolders(path)
+#folder.sort()
+#
+#for i in range(len(folder)):
+#    folder[i] = path + "/" + folder[i]
+#
+#
+#m.evaluate_data(folder)
+
+
+
+#f2.sortToFolderByLayer()
+
+#folders = toolbox.get_subfolders()
+#
+#nn.init()
+#nn.nn_main()
+#nn.nn_all_layers()
+
 
 #folder, path, layer = me.f2_main("", 0)
 #for i in range(1,10):
